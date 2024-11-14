728c4811a01549d6faa1d8c3211faeadb81849a4
aneneme <148064049+aneneme@users.noreply.github.com>
2024-11-14 08:43:38 -0300

    .

--- graficos/quantidadedeUsuarios.js
+++ graficos/quantidadedeUsuarios.js
@@ -1,1 +1,17 @@
+async function quantidadedeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
+
+}
+
+    Const data = [
+        {
+            x: nomeDasRedes,
+            y: quantidadeDeUsuarios,
+            type: 'bar'
+        }
+    ]
 
